7fbbc3321d2ba2b67fbf5f5302e193a0771d0816
heshibo1994 <csuheshibo@163.com>
2021-07-17 23:51:07 +0800

    测试mybatis-plus

--- mall-product/src/main/java/com/example/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/example/mall/product/MallProductApplication.java
@@ -1,8 +1,22 @@
 package com.example.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1)导入依赖
+ *  mybatis-plus
+ * 2)配置
+ *  配置数据源
+ *      导入数据源驱动
+ *      在application.yml配置数据源信息
+ *  配置mybatis-plus
+ *      使用MapperScan
+ *      告诉自定义sql的地址
+ *
+ */
+@MapperScan("com.example.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
--- mall-product/src/test/java/com/example/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/example/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.mall.product;
 
+import com.example.mall.product.entity.BrandEntity;
+import com.example.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.diagnostics.analyzer.BeanNotOfRequiredTypeFailureAnalyzer;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("");
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
