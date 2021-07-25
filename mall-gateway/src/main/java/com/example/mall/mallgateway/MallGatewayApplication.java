0013ca523cc40cfdfaf1c6b9438803487df2c1bf
heshibo1994 <csuheshibo@163.com>
2021-07-25 14:42:07 +0800

    网关设置

--- mall-gateway/src/main/java/com/example/mall/mallgateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/example/mall/mallgateway/MallGatewayApplication.java
@@ -2,8 +2,15 @@ package com.example.mall.mallgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+/*
+* 1.开启服务的注册发现
+*   配合Nacos的注册中心和配置中心
+* */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MallGatewayApplication {
 
     public static void main(String[] args) {
